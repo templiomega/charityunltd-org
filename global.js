db14bb3daea34cd7ecaa1477ab0521f9b61f2bb8
templiomega <templiomega@gmail.com>
2026-09-22 04:29:40 -0400

    Add files via upload

---
+++ global.js
@@ -0,0 +1,20 @@
+const hyperlink_btns = document.querySelectorAll(".hyperlinked");
+hyperlink_btns.forEach(element => {
+    switch (element.dataset.link) {
+        case ("0"):
+            element.addEventListener("click", () => {
+                window.location.href = "./index.html";
+            });
+            break;
+        case ("1"):
+            element.addEventListener("click", () => {
+                window.location.href = "./about.html";
+            });
+            break;
+        case ("2"):
+            element.addEventListener("click", () => {
+                window.open("https://www.paypal.com/ncp/payment/BQD7PF6RFQW4E", "_blank");
+            });
+            break;
+    }
+});
